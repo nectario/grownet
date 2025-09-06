148c9d81cf0e649578441faddafcd13234a255e8
Nektarios Kalogridis <mail@nektarios.com>
2025-09-06 13:38:13 -0400

    Stop tracking JavaProject; src/java is primary via symlink (ignored)

--- JavaProject/GrowNet/src/main/java/ai/nektron/grownet/FireHook.java
+++
@@ -1,7 +0,0 @@
-package ai.nektron.grownet;
-
-/** Callback invoked after a neuron fires. */
-@FunctionalInterface
-public interface FireHook {
-    void onFire(double inputValue, Neuron neuron);
-}
